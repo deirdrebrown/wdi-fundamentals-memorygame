6f8433f36dfaf7c487349d8aa9f50ed27492ca5d
Deirdre P Brown <DeirdrePBrown@Deirdres-Air.fios-router.home>
2019-09-02 15:52:44 -0400

    added matching logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"]
+cardsInPlay = [ ]
 
-console.log("User flipped" + cardOne);
+const cardOne = cards[0]
+cardsInPlay.push('cardOne');
+console.log("User flipped" + " " + cardOne)
+	
+const cardTwo = cards[2]
+cardsInPlay.push('cardTwo');
+console.log("User flipped" + " " + cardTwo)	
 
+//checking if two cards have been played
+if (cardsInPlay.length === 2){
+		//check it the crads match
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");		
+	} else {
+	alert("Sorry, try again.");
+			}
+		}
+	
