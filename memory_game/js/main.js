3a56ee124ffddfdda123bd2ea5857489ee05de08
Deirdre P Brown <DeirdrePBrown@Deirdres-Air.fios-router.home>
2019-09-02 16:16:31 -0400

    added flip card function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,29 @@
 const cards = ["queen", "queen", "king", "king"]
 cardsInPlay = [ ]
 
-const cardOne = cards[0]
-cardsInPlay.push('cardOne');
-console.log("User flipped" + " " + cardOne)
-	
-const cardTwo = cards[2]
-cardsInPlay.push('cardTwo');
-console.log("User flipped" + " " + cardTwo)	
 
-//checking if two cards have been played
+
+//check if two cards have been played
 if (cardsInPlay.length === 2){
-		//check it the crads match
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+		}
+
+
+//check for a match	
+var checkForMatch = function (){
+if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");		
 	} else {
 	alert("Sorry, try again.");
 			}
-		}
-	
+
+}
+
+// user flips card
+	function flipCard (cardId){
+		console.log("User flipped" + " " + cards[cardId]);
+		cardsInPlay.push(cards[cardId]);
+		checkForMatch;
+	}
+//calls the fliped card
+	flipCard(0);
+	flipCard(2);
